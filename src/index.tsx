115358245e2d16d8d3e5c08e9f6a83f4a842fc5a
Andrey Vovk <74706181+AndeyVovk@users.noreply.github.com>
2022-11-11 01:28:05 +0200

    push part project

--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,6 @@ import store from "./components/store";
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-
 root.render(
     <React.StrictMode>
         <Provider store={store}>
